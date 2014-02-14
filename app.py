ef506d0bb33398c6baa44782606db3db7c3f3196
roboton <roberton@gmail.com>
2014-02-14 15:13:35 -0800

    - Update filter syntax to SQLAlchemy 0.9 - set host to 0.0.0.0 for external network visiblity - Fix tabindex quirk in signup rocess

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def login():
         project=[]
         userfound=False;
         from models import Project, User
-        u=User.query.filter(User.username==username,User.password==password).first()
+        u=User.query.filter(User.username==username).filter(User.password==password).first()
         #u=db_session.query(q)
         #print u.id
         if u is not None:
@@ -300,5 +300,5 @@ def shutdown_session(exception=None):
 
 if __name__=="__main__":
     app.debug=True
-    app.run(port=8080)
+    app.run(host="0.0.0.0", port=8080)
 
