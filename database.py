6fb3936edfdb791780709be2f133e93d949d54b9
Priya Iyer <priya_iyer@berkeley.edu>
2014-02-24 13:26:32 -0800

    Merging changes with the group repo

--- database.py
+++ database.py
@@ -7,8 +7,8 @@ username="jbqpsxeonpaptz"
 password="O5lVxebs56qLeUTv9YotCU0Z3Z"
 db="df2k3fqdn06r9c"
 host="ec2-174-129-197-200.compute-1.amazonaws.com"
-SQLALCHEMY_DATBASE_URI='postgresql+psycopg2://'+username+':'+password+'@'+host+'/'+db
-#SQLALCHEMY_DATBASE_URI='mysql://rm:rm@localhost/readymade'
+#SQLALCHEMY_DATBASE_URI='postgresql+psycopg2://'+username+':'+password+'@'+host+'/'+db
+SQLALCHEMY_DATBASE_URI='mysql://rm:rm@localhost/readymade'
 engine = create_engine(SQLALCHEMY_DATBASE_URI, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
